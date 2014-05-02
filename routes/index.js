726d9d55a0cf5aa6753833b939e956e0dfcb4d54
Pedro Jimenez <pedroj.1822@gmail.com>
2014-05-02 18:21:52 -0500

    Se paso la verificacion de si ha iniciado sesion de la vista index.jade al controlador index.js, este es el que decide si rederear la vista index.jade o home.jade

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,22 @@
 exports.index = function(req, res){
   // Renderiza la plantilla 'index' cuando en el navegador
   // nos encontremos en la raiz '/' --> http://localhost:puerto/
-  res.render('index', {
-    // Enviamos como variables un título
-    // y objeto 'user' que contiene toda
-    // la información del usuario y viaja en el 'request'
-    title: 'Ejemplo de Passport JS',
-    user: req.user
-  });
+  if(req.user && req.user != "undefined"){
+    // Si se ha logrado obtener un usuario por la autenticacion de facebook o twitter
+    // se renderea la plantilla home.jade
+    res.render('home', {
+      // Enviamos como variables un título
+      // y un objeto 'user' que contiene toda
+      // la información del usuario y viaja en el 'request'
+      title: 'Ejemplo de Passport JS',
+      user: req.user
+    });
+  }else{
+    // Si NO se ha logrado obtener un usuario por la autenticacion de facebook o twitter
+    // se renderea la plantilla index.jade para que inicie sesion
+    res.render('index', {
+      // Enviamos como variables solo el título
+      title: 'Ejemplo de Passport JS'
+    });
+  }
 };
