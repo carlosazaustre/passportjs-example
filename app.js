2dbed15714eb4538d256040118338ec2b9dcd85a
Marco <cybermarkus1@gmail.com>
2017-06-20 20:24:35 +0200

    updated deps

--- app.js
+++ app.js
@@ -1,85 +1,64 @@
-// Archivo principal del Backend, configuración del servidor
-// y otras opciones
-
-var express = require('express'); // Express: Framework HTTP para Node.js
-var routes = require('./routes'); // Dónde tenemos la configuración de las rutas
+var express = require('express'); // Express: Framework HTTP
+// Routes
+var routes = require('./server/routes'); 
+var authRoutes = require('./server/auth');
 var path = require('path');
 
-var mongoose = require('mongoose'); // Mongoose: Libreria para conectar con MongoDB
-var passport = require('passport'); // Passport: Middleware de Node que facilita la autenticación de usuarios
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session')
 
-// Importamos el modelo usuario y la configuración de passport
-require('./models/user');
-require('./passport')(passport);
+var mongoose = require('mongoose'); // Mongoose: MongoDB
+var passport = require('passport'); // Passport: Auth Middleware
 
-// Conexión a la base de datos de MongoDB que tenemos en local
+// user model and passport conf
+require('./data/user');
+require('./server/passport')(passport);
+
+// Connect with MongoDB
 mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
   if(err) throw err;
   console.log('Conectado con éxito a la BD');
 });
 
-// Iniciamos la aplicación Express
+// Init express app
 var app = express();
 
-// Configuración (Puerto de escucha, sistema de plantillas, directorio de vistas,...)
+// Conf
 app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
+app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(methodOverride());
 
-// Middlewares de Express que nos permiten enrutar y poder
-// realizar peticiones HTTP (GET, POST, PUT, DELETE)
-app.use(express.cookieParser());
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(express.methodOverride());
+// Middlewares HTTP (GET, POST, PUT, DELETE)
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Ruta de los archivos estáticos (HTML estáticos, JS, CSS,...)
+// Static route
 app.use(express.static(path.join(__dirname, 'public')));
-// Indicamos que use sesiones, para almacenar el objeto usuario
-// y que lo recuerde aunque abandonemos la página
-app.use(express.session({ secret: 'lollllo' }));
+// Session handler
+app.use(session({ 
+  secret: 'keyboard cat', 
+  cookie: { maxAge: 60000 }, 
+  resave: true, 
+  saveUninitialized: true 
+}))
 
-// Configuración de Passport. Lo inicializamos
-// y le indicamos que Passport maneje la Sesión
+// Passport to control on the session
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(app.router);
 
-// Si estoy en local, le indicamos que maneje los errores
-// y nos muestre un log más detallado
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-/* Rutas de la aplicación */
-// Cuando estemos en http://localhost:puerto/ (la raiz) se ejecuta el metodo index
-// del modulo 'routes'
+// Routes
 app.get('/', routes.index);
+authRoutes(app, passport);
 
-/* Rutas de Passport */
-// Ruta para desloguearse
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
-});
-// Ruta para autenticarse con Twitter (enlace de login)
-app.get('/auth/twitter', passport.authenticate('twitter'));
-// Ruta para autenticarse con Facebook (enlace de login)
-app.get('/auth/facebook', passport.authenticate('facebook'));
-// Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
-// En caso de fallo redirige a otra vista '/login'
-app.get('/auth/twitter/callback', passport.authenticate('twitter',
-  { successRedirect: '/', failureRedirect: '/login' }
-));
-// Ruta de callback, a la que redirigirá tras autenticarse con Facebook.
-// En caso de fallo redirige a otra vista '/login'
-app.get('/auth/facebook/callback', passport.authenticate('facebook',
-  { successRedirect: '/', failureRedirect: '/login' }
-));
-
-// Inicio del servidor
+// Init
 app.listen(app.get('port'), function(){
-  console.log('Aplicación Express escuchando en el puerto ' + app.get('port'));
+  console.log('Express app listening on port ' + app.get('port'));
 });
--- server/passport.js
+++ server/passport.js
@@ -7,7 +7,31 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 // Fichero de configuración donde se encuentran las API keys
 // Este archivo no debe subirse a GitHub ya que contiene datos
 // que pueden comprometer la seguridad de la aplicación.
-var config = require('./config');
+var config = require('../config');
+
+// Este método sirve como el callback para los TwitterStrategy y FacebookStrategy
+function authenticationPassport(accessToken, refreshToken, profile, done) {
+	// Busca en la base de datos si el usuario ya se autenticó en otro
+	// momento y ya está almacenado en ella
+	User.findOne({provider_id: profile.id}, function(err, user) {
+		if(err) throw(err);
+		// Si existe en la Base de Datos, lo devuelve
+		if(!err && user!= null) return done(null, user);
+
+		// Si no existe crea un nuevo objecto usuario
+		var user = new User({
+			provider_id	: profile.id,
+			provider	: profile.provider,
+			name		: profile.displayName,
+			photo		: profile.photos[0].value
+		});
+		//...y lo almacena en la base de datos
+		user.save(function(err) {
+			if(err) throw err;
+			done(null, user);
+		});
+	});
+};
 
 // Exportamos como módulo las funciones de passport, de manera que
 // podamos utilizarlas en otras partes de la aplicación.
@@ -31,28 +55,7 @@ module.exports = function(passport) {
 		consumerKey		 : config.twitter.key,
 		consumerSecret	: config.twitter.secret,
 		callbackURL		 : '/auth/twitter/callback'
-	}, function(accessToken, refreshToken, profile, done) {
-		// Busca en la base de datos si el usuario ya se autenticó en otro
-		// momento y ya está almacenado en ella
-		User.findOne({provider_id: profile.id}, function(err, user) {
-			if(err) throw(err);
-			// Si existe en la Base de Datos, lo devuelve
-			if(!err && user!= null) return done(null, user);
-
-			// Si no existe crea un nuevo objecto usuario
-			var user = new User({
-				provider_id	: profile.id,
-				provider		 : profile.provider,
-				name				 : profile.displayName,
-				photo				: profile.photos[0].value
-			});
-			//...y lo almacena en la base de datos
-			user.save(function(err) {
-				if(err) throw err;
-				done(null, user);
-			});
-		});
-	}));
+	}, authenticationPassport));
 
 	// Configuración del autenticado con Facebook
 	passport.use(new FacebookStrategy({
@@ -60,29 +63,10 @@ module.exports = function(passport) {
 		clientSecret	: config.facebook.secret,
 		callbackURL	 : '/auth/facebook/callback',
 		profileFields : ['id', 'displayName', /*'provider',*/ 'photos']
-	}, function(accessToken, refreshToken, profile, done) {
 		// El campo 'profileFields' nos permite que los campos que almacenamos
 		// se llamen igual tanto para si el usuario se autentica por Twitter o
 		// por Facebook, ya que cada proveedor entrega los datos en el JSON con
 		// un nombre diferente.
 		// Passport esto lo sabe y nos lo pone más sencillo con ese campo
-		User.findOne({provider_id: profile.id}, function(err, user) {
-			if(err) throw(err);
-			if(!err && user!= null) return done(null, user);
-
-			// Al igual que antes, si el usuario ya existe lo devuelve
-			// y si no, lo crea y salva en la base de datos
-			var user = new User({
-				provider_id	: profile.id,
-				provider		 : profile.provider,
-				name				 : profile.displayName,
-				photo				: profile.photos[0].value
-			});
-			user.save(function(err) {
-				if(err) throw err;
-				done(null, user);
-			});
-		});
-	}));
-
+	}, authenticationPassport));
 };
